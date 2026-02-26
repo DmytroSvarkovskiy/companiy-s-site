3d2cd0dd0cafa29daabd1ee8f388de1bcbb3f65f
Dmytro Svarkovskiy <svarkovskii722@gmail.com>
2026-02-26 23:22:03 +0200

    done projects block

--- widgets/OurProducts/OurProducts.tsx
+++ widgets/OurProducts/OurProducts.tsx
@@ -1,11 +1,13 @@
 import { TitleSection } from "@/entities/index.client";
 import { getScopedI18n } from "@/lib";
+import { ProductsList } from "./ui/index.client";
 
 export const OurProducts = async () => {
   const t = await getScopedI18n("productsSection");
   return (
     <section className="container section">
       <TitleSection title={t("title")} subtitle={t("subtitle")} />
+      <ProductsList />
     </section>
   );
 };
---
+++ widgets/OurProducts/ui/ProductsList/data.ts
@@ -0,0 +1,69 @@
+import { useScopedI18n } from "@/lib/index.client";
+
+type TProductCard = {
+  key: "restup" | "ecommerce";
+  eyebrow: string;
+  title: string;
+  description: string;
+  src: string;
+  stats: {
+    establishedAudience: { label: string; value: string; deltaLabel: string };
+    involved: { label: string; value: string; deltaLabel: string };
+    googlePlayRating: { label: string; value: string };
+  };
+};
+
+export const useProjectsData = () => {
+  const t = useScopedI18n("productsSection");
+
+  const productsCards = [
+    {
+      key: "restup",
+      eyebrow: t("restup.eyebrow"),
+      title: t("restup.title"),
+      description: t("restup.description"),
+      src: "/projects/productsRestup.png",
+      stats: {
+        establishedAudience: {
+          label: t("stats.establishedAudience"),
+          value: t("stats.usersCount", { count: 567 }),
+          deltaLabel: `+3,5% ▲ ${t("stats.vs30DaysAgo")}`,
+        },
+        involved: {
+          label: t("stats.involved"),
+          value: t("stats.newUsers", { count: 92 }),
+          deltaLabel: `+3,5% ▲ ${t("stats.vs30DaysAgo")}`,
+        },
+        googlePlayRating: {
+          label: t("stats.googlePlayRating"),
+          value: "4.9",
+        },
+      },
+    },
+    {
+      key: "ecommerce",
+      eyebrow: t("ecommerce.eyebrow"),
+      title: t("ecommerce.title"),
+      description: t("ecommerce.description"),
+      src: "/projects/productsEcom.png",
+      stats: {
+        establishedAudience: {
+          label: t("stats.establishedAudience"),
+          value: t("stats.usersCount", { count: 241 }),
+          deltaLabel: `+2,5% ▲ ${t("stats.vs30DaysAgo")}`,
+        },
+        involved: {
+          label: t("stats.involved"),
+          value: t("stats.newUsers", { count: 41 }),
+          deltaLabel: `+1,5% ▲ ${t("stats.vs30DaysAgo")}`,
+        },
+        googlePlayRating: {
+          label: t("stats.googlePlayRating"),
+          value: "4.7",
+        },
+      },
+    },
+  ] satisfies TProductCard[];
+
+  return productsCards;
+};
