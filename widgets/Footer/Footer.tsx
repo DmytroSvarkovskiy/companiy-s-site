84d50ac3a0dc93af0c3ad52e06f1241a37706076
Dmytro Svarkovskiy <svarkovskii722@gmail.com>
2026-03-17 13:45:56 +0200

    updated footer

--- widgets/Footer/Footer.tsx
+++ widgets/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { Mail, MapPin, Phone } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Logo } from "@/entities";
@@ -11,12 +12,19 @@ export const Footer = async () => {
     { id: "behance", href: COMPANY_LINKS.beehance, icon: "/Icons/be.svg", alt: "behance" },
     { id: "linkedin", href: COMPANY_LINKS.linkedin, icon: "/Icons/linkedin.svg", alt: "linkedin" },
   ];
-  const CONTACTS = [];
+  const CONTACTS = [
+    { type: "email", value: COMPANY_LINKS.email },
+    { type: "phone", value: COMPANY_LINKS.phone1 },
+    { type: "phone", value: COMPANY_LINKS.phone2 },
+  ] as const;
+
+  const subtitleClassName = "font-medium text-24 mb-6";
+
   return (
     <div className="section">
       <footer className="border-t dark:border-phone-btn border-muted">
         <div
-          className="container py-16 flex flex-col
+          className="container py-16 flex flex-col gap-8
         md:pb-14
         xl:bp-6"
         >
@@ -40,14 +48,69 @@ export const Footer = async () => {
             </div>
           </div>
           <div>
-            <p>{t("contact")}</p>
             <div>
-              <span></span>
-              <span></span>
-              <span></span>
+              <p className={subtitleClassName}>{t("contact")}</p>
+              <div className="flex flex-col gap-8">
+                <div className="text-menu-foreground flex flex-col gap-2">
+                  {CONTACTS?.map((el) => {
+                    const href = el.type === "phone" ? `tel:${el.value}` : `mailto:${el.value}`;
+                    return (
+                      <span key={el.value} className="flex items-center gap-2 text-16">
+                        {el.type === "phone" ? (
+                          <Phone className="size-5" />
+                        ) : (
+                          <Mail className="size-5" />
+                        )}
+                        <Link href={href}>{el.value}</Link>
+                      </span>
+                    );
+                  })}
+                </div>
+                <div>
+                  <p className={subtitleClassName}>{t("trusted_by")}</p>
+                  <div className="flex gap-5">
+                    <div
+                      className="
+      w-16.25 h-12
+      bg-blue-600 dark:bg-white
+      mask-[url('/footer/ebrr.png')]
+      mask-contain
+      mask-no-repeat
+      mask-center
+      [-webkit-mask-image:url('/footer/ebrr.png')]
+      [-webkit-mask-size:contain]
+      [-webkit-mask-repeat:no-repeat]
+      [-webkit-mask-position:center]
+    "
+                    />
+                    <div
+                      className="
+    w-53.5 h-12
+    bg-foreground dark:bg-white
+    mask-[url('/footer/mtu.png')] mask-contain mask-no-repeat mask-center
+    [-webkit-mask-image:url('/footer/mtu.png')]
+    [-webkit-mask-size:contain]
+    [-webkit-mask-repeat:no-repeat]
+    [-webkit-mask-position:center]
+  "
+                    />{" "}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div></div>
+          <div>
+            <p className={subtitleClassName}>{t("location")}</p>
+            <Link
+              href="https://www.google.com/maps?q=50.90774463102274,34.79178114673439"
+              target="_blank"
+              rel="noopener noreferrer"
+              className=" hover:underline flex items-center gap-2 text-menu-foreground"
+            >
+              <MapPin />
+              <span>1-ша Набережна річки Стрілки, 9А, Суми, Україна</span>
+            </Link>
+          </div>
         </div>
       </footer>
     </div>
